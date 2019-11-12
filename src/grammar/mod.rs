d5dc599ffe4d5bb817394059097bd529cbbef7a7
Liu Xiaoyi <circuitcoder0@gmail.com>
2019-11-12 16:28:35 +0800

    Splited solid type and inferring type to simplify typing

--- src/grammar/mod.rs
+++ src/grammar/mod.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::rc::Rc;
 
 #[derive(Debug)]
 pub enum IntSuffix {
@@ -64,7 +65,7 @@ pub enum TypeSegArgs<'a> {
 }
 
 #[derive(Debug)]
-pub enum Type<'a> {
+pub enum SolidType<'a> {
   Ident(TypePath<'a>),
   Tuple(Vec<Type<'a>>),
   Ref(bool, Box<Type<'a>>),
@@ -74,7 +75,19 @@ pub enum Type<'a> {
   BareFunc(FnTypeSpec<'a>),
 
   Never,
+}
+
+#[derive(Debug)]
+pub enum Type<'a> {
+  Solid(SolidType<'a>),
+  SameAs(Rc<Type<'a>>),
   Placeholder,
 }
 
-pub const UNIT_TYPE: Type = Type::Tuple(Vec::new());
+impl<'a> From<SolidType<'a>> for Type<'a> {
+  fn from(f: SolidType<'a>) -> Self {
+    Self::Solid(f)
+  }
+}
+
+pub const UNIT_TYPE: Type = Type::Solid(SolidType::Tuple(Vec::new()));
--- src/parser/type.rs
+++ src/parser/type.rs
@@ -30,12 +30,12 @@ named!(
     ),
     |(_, r, _)| {
       match r {
-        None => Type::Tuple(Vec::new()),
+        None => UNIT_TYPE,
         Some((first, mut rest)) => {
           let mut v = Vec::with_capacity(rest.len() + 1);
           v.push(first);
           v.append(&mut rest);
-          Type::Tuple(v)
+          SolidType::Tuple(v).into()
         },
       }
     }
@@ -49,7 +49,7 @@ named!(ref_type<&str, Type>,
       opt!(tag!(keywords::KW_MUT)),
       type_no_bound
     )),
-    |(_, m, t)| Type::Ref(m.is_some(), Box::new(t))
+    |(_, m, t)| SolidType::Ref(m.is_some(), Box::new(t)).into()
   )
 );
 
@@ -63,7 +63,7 @@ named!(array_type<&str, Type>,
       literal,
       tag!("]")
     )),
-    |(_, t, _, n, _)| Type::Array(Box::new(t), 0)
+    |(_, t, _, n, _)| SolidType::Array(Box::new(t), 0).into() // TODO: compute length
   )
 );
 
@@ -74,7 +74,7 @@ named!(slice_type<&str, Type>,
       r#type,
       tag!("]")
     )),
-    |t| Type::Slice(Box::new(t))
+    |t| SolidType::Slice(Box::new(t)).into()
   )
 );
 
@@ -105,10 +105,10 @@ named!(bare_func<&str, Type>,
       opt!(complete!(bare_func_ret))
     )),
     |(_, _, p, _, r)| {
-      Type::BareFunc(FnTypeSpec {
+      SolidType::BareFunc(FnTypeSpec {
         args: p,
         ret: Box::new(r.unwrap_or_else(|| UNIT_TYPE)),
-      })
+      }).into()
     }
   )
 );
@@ -121,10 +121,10 @@ named!(
     | array_type
     | slice_type
     | map!(tag!("_"), |_| Type::Placeholder)
-    | map!(tag!("!"), |_| Type::Never)
+    | map!(tag!("!"), |_| SolidType::Never.into())
     | bare_func
     | mrws!(delimited!(tag!("("), r#type, tag!(")")))
-    | map!(type_path, Type::Ident)
+    | map!(type_path, |p| SolidType::Ident(p).into())
   )
 );
 
